9663a1a4b7e68b903fb899ff81a58cbaeb9910ab
Jason J. Ayala P <JasonJAyalaP+Github@Gmail.com>
2020-07-28 06:38:15 -0500

    Update make self extract.bat

--- 7zip/make self extract.bat
+++ 7zip/make self extract.bat
@@ -1,3 +1,3 @@
 7za a -m0=Copy ..\Output\Installer ..\Output\
-copy /b 7zSD.sfx + config.txt + ..\Output\Installer.7z ..\Output\Install-Whonix-sfx.exe
+copy /b 7zSD.sfx + config.txt + ..\Output\Installer.7z ..\Output\whonix-installer.sfx.exe
 pause
